75a228eb60e1b49e5e501bd8d5cf87ea90f09d11
Piotr <piotrryszewski@gmail.com>
2016-07-14 22:03:51 +0200

    Geolocation

---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(fucntion(){
+if (navigator.geolocation) {
+ navigator.geolocation.getCurrentPosition(function(position) {
+   $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+ });
+}})
