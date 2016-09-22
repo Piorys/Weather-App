3f61abd18cfbcf2f37f51367bf398e407bc47ed1
Piotr <piotrryszewski@gmail.com>
2016-09-22 22:03:55 +0200

    Almost there
    After a short while of absence in js world I finally managed to get to work. 2 of 3 user stories completed

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-$(document).ready(fucntion(){
-if (navigator.geolocation) {
- navigator.geolocation.getCurrentPosition(function(position) {
-   $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
- });
-}})
+//User location
+$.getJSON('http://ip-api.com/json', function(data){
+var lat = data.lat;
+var lon = data.lon;
+var units = "metric";
+
+$("#city").html(data.city + ", " + data.country)
+console.log(data);
+//Open weather API request
+$.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=' + units + '&APPID=e95d958a11128b11ad3eb0fa101dae38', function(json){
+console.log(json);
+$("#temperature").html(json.main.temp + ' C&deg');
+$("#humidity").html(json.main.humidity + ' %');
+$("#overall").html(json.weather[0].main);
+$("#icon").html('<img src="http://openweathermap.org/img/w/' + json.weather[0].icon + '.png"</img>')
+})
+})
