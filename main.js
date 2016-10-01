b4af8217598a60b9f5d811bb11dd3a62b71d9e98
Piotr <piotrryszewski@gmail.com>
2016-10-01 13:26:59 +0200

    Finished

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 //User location
 $.getJSON('http://ip-api.com/json', function(data){
 var lat = data.lat;
@@ -9,9 +10,10 @@ console.log(data);
 //Open weather API request
 $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=' + units + '&APPID=e95d958a11128b11ad3eb0fa101dae38', function(json){
 console.log(json);
-$("#temperature").html(json.main.temp + ' C&deg');
+$("#temperature-celcius").html(json.main.temp + ' C&deg');
+$("#temperature-farenheit").html((json.main.temp * 1,8 + 32) + ' F&deg');
 $("#humidity").html(json.main.humidity + ' %');
 $("#overall").html(json.weather[0].main);
-$("#icon").html('<img src="http://openweathermap.org/img/w/' + json.weather[0].icon + '.png"</img>')
-})
-})
+$("#icon").html('<img src="http://openweathermap.org/img/w/' + json.weather[0].icon + '.png"</img>');
+});
+});
